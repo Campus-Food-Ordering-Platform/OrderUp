0fb5c100f47e68cfce483cebf8337958dfeb26bd
Segun Ajibola <naomimareno05@gmail.com>
2026-04-05 15:40:37 +0200

    Setup:React+ Vite+Tailwind+React Router

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+﻿import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div className='p-8 text-center text-2xl'>Welcome to OrderUp - Coming Soon</div>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
