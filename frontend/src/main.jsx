9278730b5eed4992521ae614955b99ae45cfcdf3
Jonaphan-Mareno <jonamareno111@gmail.com>
2026-04-12 23:24:03 +0200

    fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,8 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin,
+        redirect_uri: `${window.location.origin}/auth/callback`,
       }}
+      cacheLocation="localstorage"
     >
       <App />
     </Auth0Provider>
