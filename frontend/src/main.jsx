034c4d27f0adeced8b41647f991f9d1d0df03e16
Siyanqoba Kunene <164580198+Siyanqoba-Kunene@users.noreply.github.com>
2026-04-12 19:26:05 +0200

    fixed bug(turnsout there is a difference between this ` and this ' )

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,8 +10,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: '${window.location.origin}/auth/callback',// issue was here it was redirecting to the wrong page, when logged in with google we redirect back to welcome page instead of the callback which checks the database.
-      }}
+        redirect_uri: `${window.location.origin}/auth/callback`,// issue was here it was redirecting to the wrong page, when logged in with google we redirect back to welcome page instead of the callback which checks the database.
+      }}//also the issue was i used this syntax '' instead of `` for the redirect uri and it was not working because it was not able to parse the environment variable and the window.location.origin.=
       cacheLocation='localstorage'// we did not add this line before and it was causing the user to be logged out when they refresh the page, because the default cache location is memory and it does not persist the session after a page refresh.
       
     >
